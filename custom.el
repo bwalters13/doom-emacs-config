98fefd60601c2d1b9b118700a4d0641ec9f95212
bwalters13 <blakewalterss@gmail.com>
2025-06-18 11:48:23 -0400

    update default theme

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(doom-cyan-charcoal))
+ '(custom-enabled-themes '(doom-Iosvkem))
  '(custom-safe-themes
    '("ff53a30172ba57f16a4c0f477ad496b45adc7de23608cc73c501f16e06d4779d"
      "4d714a034e7747598869bef1104e96336a71c3d141fa58618e4606a27507db4c"
